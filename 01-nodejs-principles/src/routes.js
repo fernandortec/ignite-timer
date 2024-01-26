16f6c4853bfabf2fff56351b8e9d9a6bce808db4
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-01-26 17:38:46 -0300

    add regex and delete method

--- 01-nodejs-principles/src/routes.js
+++ 01-nodejs-principles/src/routes.js
@@ -1,17 +1,18 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
-    method: 'GET', path: "/users", handler: (req, res) => {
+    method: 'GET', path: buildRoutePath("/users"), handler: (req, res) => {
       const users = database.select("users")
       return res.end(JSON.stringify(users))
     }
   },
   {
-    method: "POST", path: "/users", handler: (req, res) => {
+    method: "POST", path: buildRoutePath("/users"), handler: (req, res) => {
       const { name, email } = req.body
 
       const user = {
@@ -25,5 +26,28 @@ export const routes = [
       return res.writeHead(201).end()
     }
 
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: "PUT",
+    path: buildRoutePath("/users/:id"),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, { name, email })
+
+      return res.writeHead(204).end()
+    }
   }
 ]
