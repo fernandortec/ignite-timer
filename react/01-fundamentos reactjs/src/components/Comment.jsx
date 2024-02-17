dcb428cad275791d39f5575a2d09f22b7307db94
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-02-16 23:08:37 -0300

    add comment list component

---
+++ react/01-fundamentos reactjs/src/components/Comment.jsx
@@ -0,0 +1,40 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./comment.module.css";
+export function Comment() {
+	return (
+		<div className={styles.comment}>
+			<img src="https://github.com/fernandortec.png" alt="" />
+
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Fernando Rodrigues</strong>
+							<time
+								title="11 de maio as 8h13min"
+								dateTime="2024-05-11 08:13:30"
+							>
+								Cerca de 1h atrás
+							</time>
+						</div>
+
+						<button title="Deletar comentário" type="button">
+							<Trash size={20} />
+						</button>
+					</header>
+
+					<p>Muito bom Devon, parabéns!! 👏👏👏</p>
+				</div>
+
+				<footer>
+					<button type="button">
+						<ThumbsUp />
+						Aplaudir <span>20</span>
+					</button>
+				</footer>
+
+				<footer>Aplaudir</footer>
+			</div>
+		</div>
+	);
+}
