76ca65a7ae91fc5ca30e3cb0eff53c6a42a0b052
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-02-27 18:56:23 -0300

    add default layout, routering and pages

--- react/02-ignite-timer/src/main.tsx
+++ react/02-ignite-timer/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App.tsx'
 
-// biome-ignore lint/style/noNonNullAssertion: 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-);
+// biome-ignore lint/style/noNonNullAssertion:
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
